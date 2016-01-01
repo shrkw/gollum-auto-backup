8ccb68abdaadf1898427f7c910fec6adf5958b2f
shrkw <shrkwh@gmail.com>
2016-01-01 23:20:18 +0900

    Split extending Precious module part

---
+++ precious.rb
@@ -0,0 +1,26 @@
+module Precious
+  class App < Sinatra::Base
+    basic_auth_username = ENV['BASIC_AUTH_USERNAME']
+    basic_auth_password = ENV['BASIC_AUTH_PASSWORD']
+    if basic_auth_username && basic_auth_password
+      before do
+        authorize
+      end
+    end
+
+    private
+    def authorize
+      response['WWW-Authenticate'] = %(Basic realm="Restricted area.")
+      throw(:halt, [401, "Not authorized\n"]) unless authorized?
+    end
+
+    def authorized?
+      basic_auth_username = ENV['BASIC_AUTH_USERNAME']
+      basic_auth_password = ENV['BASIC_AUTH_PASSWORD']
+
+      @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [basic_auth_username, basic_auth_password]
+    end
+  end
+end
+
