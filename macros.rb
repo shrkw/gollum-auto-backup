ab578af029fed123a1fb9531be83381f712e84e8
shrkw <shrkwh@gmail.com>
2016-01-03 21:23:57 +0900

    Show modified date

--- macros.rb
+++ macros.rb
@@ -11,8 +11,9 @@ module Gollum
       def render(length='10')
         if @wiki.pages.size > 0
           pages = @wiki.pages.sort_by { |p| p.versions[0].authored_date }.reverse.slice(0, length.to_i)
-          '<ul id="pages">' + pages.map { |p| "<li><a href='#{p.url_path_display}'>#{p.name}</a></li>" }.join + '</ul>'
+          result = '<ul>' + pages.map { |p| "<li><a href=\"#{p.url_path}\">#{p.url_path_display}</a> <span class=\"tiny\">#{p.versions[0].authored_date}</span></li>" }.join + '</ul>'
         end
+        "<div class=\"toc\"><div class=\"toc-title\">Order by Last Modified Limit (#{length})</div>#{result}</div>"
       end
     end
   end
