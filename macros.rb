4826b19be5460bc3a2eab83d1924ff714a26ab42
shrkw <shrkwh@gmail.com>
2016-01-03 19:09:24 +0900

    limit list length

--- macros.rb
+++ macros.rb
@@ -8,9 +8,9 @@ module Gollum
       end
     end
     class AllPagesLastUpdated < Gollum::Macro
-      def render
+      def render(length='10')
         if @wiki.pages.size > 0
-          pages = @wiki.pages.sort_by { |p| p.versions[0].authored_date }.reverse
+          pages = @wiki.pages.sort_by { |p| p.versions[0].authored_date }.reverse.slice(0, length.to_i)
           '<ul id="pages">' + pages.map { |p| "<li><a href='#{p.url_path_display}'>#{p.name}</a></li>" }.join + '</ul>'
         end
       end
